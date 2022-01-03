3d34f2bf657db078918b0636b1c7df5311ddcca2
Khaerul Ansari <khaerulaansari@gmail.com>
2022-01-03 22:47:37 +0700

    update

--- script/script.js
+++ script/script.js
@@ -1,4 +1,11 @@
 AOS.init({
     duration: 1200,
 })
+
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".navbar-fixed-top");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    })
+})
   
